74f99d16609349ebf0ada6341c6608b5abe5b7e0
Karoline Barão <barao.karoline@gmail.com>
2022-03-31 09:29:57 -0300

    classe Departamento

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplicacao;
+
+import model.entidades.Departamento;
+
+/**
+ *
+ * @author Karol Dev
+ */
+public class Programa {
+    public static void main(String[] args) {
+        Departamento obj = new Departamento(1,"Books");
+        System.out.println(obj);
+    }
+}
---
+++ src/model/entidades/Departamento.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.entidades;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Departamento implements Serializable{
+    
+    private Integer id;
+    private String name;
+    
+    public Departamento(){
+        
+    }
+    
+    public Departamento(Integer id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 89 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Departamento other = (Departamento) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento{" + "id=" + id + ", name=" + name + '}';
+    }
+    
+    
+    
+    
+    
+
+
+
+    
+    
+}
